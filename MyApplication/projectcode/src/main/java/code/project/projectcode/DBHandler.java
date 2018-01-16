e8d8f9d64a6d9bbf36e27b53347bd869ea277b60
S-Danagher <32181224+S-Danagher@users.noreply.github.com>
2018-01-16 11:26:45 -0500

    updated mock db

--- MyApplication/projectcode/src/main/java/code/project/projectcode/DBHandler.java
+++ MyApplication/projectcode/src/main/java/code/project/projectcode/DBHandler.java
@@ -1,4 +1,4 @@
-package code.project.projectcode;
+/*package code.project.projectcode;
 
 //import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -8,6 +8,10 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database .DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 //import com.google.firebase.storage.FirebaseStorage;
 
 //import com.google.firebase.storage.StorageReference;
@@ -16,12 +20,12 @@ import com.google.firebase.database.FirebaseDatabase;
 Created By Sean Danagher
 TEAM PANADA
 PROJECT CODE
- */
+/
 
 import com.google.firebase.database.DataSnapshot;
 
 public class DBHandler {
-
+/*
 
 
     DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
@@ -51,3 +55,139 @@ public class DBHandler {
 
 }
 }
+/
+public JSONObject Tutorial1;
+public JSONObject Tutorial2;
+public JSONObject Tutorial3;
+public JSONObject Tutorial4;
+public JSONObject Tutorial5;
+
+public DBHandler() {
+
+    Tutorial1 = new JSONObject();
+    try {
+        Tutorial1.put("Question", "2");
+        Tutorial1.put("PA1", "NAME OF STUDENT2");
+        Tutorial1.put("PA2", "4rd");
+        Tutorial1.put("PA3", "scicence");
+        Tutorial1.put("PA4", "5/5/1993");
+
+    } catch (JSONException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+    JSONObject Tutorial2 = new JSONObject();
+    try {
+        Tutorial2.put("Question", "2");
+        Tutorial2.put("PA1", "NAME OF STUDENT2");
+        Tutorial2.put("PA2", "4rd");
+        Tutorial2.put("PA3", "scicence");
+        Tutorial2.put("PA4", "5/5/1993");
+
+    } catch (JSONException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+    JSONObject Tutorial3 = new JSONObject();
+    try {
+        Tutorial3.put("Question", "2");
+        Tutorial3.put("PA1", "NAME OF STUDENT2");
+        Tutorial3.put("PA2", "4rd");
+        Tutorial3.put("PA3", "scicence");
+        Tutorial3.put("PA4", "5/5/1993");
+
+    } catch (JSONException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+    JSONObject Tutorial4 = new JSONObject();
+    try {
+        Tutorial4.put("Question", "2");
+        Tutorial4.put("PA1", "NAME OF STUDENT2");
+        Tutorial4.put("PA2", "4rd");
+        Tutorial4.put("PA3", "scicence");
+        Tutorial4.put("PA4", "SoemthingElse");
+
+    } catch (JSONException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+    JSONObject Tutorial5 = new JSONObject();
+    try {
+        Tutorial5.put("Question", "2");
+        Tutorial5.put("PA1", "NAME OF STUDENT2");
+        Tutorial5.put("PA2", "4rd");
+        Tutorial5.put("PA3", "scicence");
+        Tutorial5.put("PA4", "5/5/1993");
+
+    } catch (JSONException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+
+    JSONArray jsonArray = new JSONArray();
+
+    jsonArray.put(Tutorial1);
+    jsonArray.put(Tutorial2);
+
+    JSONObject studentsObj = new JSONObject();
+    studentsObj.put("Students", jsonArray);
+
+
+
+    String jsonStr = studentsObj.toString();
+
+    System.out.println("jsonString: "+jsonStr);
+
+
+}
+
+JSONArray quizes = new JSONArray()
+    {
+        "Tutorials" : {
+        "1" : {
+        "Answer" : "...---...",
+        "PA1" : "...---...",
+        "PA2" : "---...---",
+        "PA3" : "..----..-",
+        "PA4" : "--.-.---.",
+        "Question" : "How do you represent SOS?"
+        },
+        "2" : {
+        "Answer" : ".-.",
+        "PA1" : "-.",
+        "PA2" : "--",
+        "PA3" : ".-.",
+        "PA4" : "--",
+        "Question" : "How do you represent the letter R?"
+        },
+        "3" : {
+        "Answer" : ".-",
+        "PA1" : "-.",
+        "PA2" : "--",
+        "PA3" : ".-",
+        "PA4" : "--",
+        "Question" : "How do you represent the letter A?"
+        },
+        "4" : {
+        "Answer" : ".----",
+        "PA1" : "..---",
+        "PA2" : "--...",
+        "PA3" : ".----",
+        "PA4" : "-----",
+        "Question" : "How do you represent the Number 1?"
+        },
+        "5" : {
+        "Answer" : "-....",
+        "PA1" : "...--",
+        "PA2" : "-....",
+        "PA3" : ".----",
+        "PA4" : "----.",
+        "Question" : "How do you represent the Number 6?"
+        }
+        }
+        }
+*/
