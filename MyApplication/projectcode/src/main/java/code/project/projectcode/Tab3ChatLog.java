0b1483dc090f3059546063b3a2a439b8320f5943
Joemiddle <andrewjm25@hotmail.com>
2018-03-18 20:17:29 -0400

    updat for chat

---
+++ MyApplication/projectcode/src/main/java/code/project/projectcode/Tab3ChatLog.java
@@ -0,0 +1,33 @@
+package code.project.projectcode;
+/*
+Createed By Radhika Hira
+TEAM PANAD
+PROJECT CODE
+ */
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+
+public class Tab3ChatLog extends Fragment{
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Handling View
+        final View rootView = inflater.inflate(R.layout.tab3_chatlog, container, false);
+
+
+
+
+
+
+
+        return rootView;
+    }
+
+}
